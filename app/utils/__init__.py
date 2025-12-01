9299114702b440f54ca2ee3c02ba756ea249d801
GermanMT <amtrujillo@us.es>
2025-12-01 02:05:55 +0100

    fix: delete unncesary async function into json encoder

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,32 +1,11 @@
-from .auth import (
-    SessionManager,
-    get_auth_from_request,
-    get_current_headers,
-    reset_current_headers,
-    session_pool,
-    set_current_headers,
-)
-from .logics import (
-    create_vex,
-    get_package_ssc,
-    get_package_status,
-    get_version_ssc,
-    get_version_status,
-)
-from .others import as_text_content, json_encoder
+from .json_encoder import JSONEncoder
+from .request_context import RequestContext
+from .session_manager import SessionManager
+from .session_pool import SessionPool
 
 __all__ = [
+    "JSONEncoder",
+    "RequestContext",
     "SessionManager",
-    "as_text_content",
-    "create_vex",
-    "get_auth_from_request",
-    "get_current_headers",
-    "get_package_ssc",
-    "get_package_status",
-    "get_version_ssc",
-    "get_version_status",
-    "json_encoder",
-    "reset_current_headers",
-    "session_pool",
-    "set_current_headers"
+    "SessionPool",
 ]
--- app/utils/others/__init__.py
+++
@@ -1,7 +0,0 @@
-from .as_text_content import as_text_content
-from .json_encoder import json_encoder
-
-__all__ = [
-    "as_text_content",
-    "json_encoder"
-]
